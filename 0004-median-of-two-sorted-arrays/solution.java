33ce6c264e59a92bfc03e34c5cc9491b856205f7
Manojraj <manojraj5423@gmail.com>
2026-03-07 06:49:16 +0000

    [LeetCode Sync] Runtime - 2 ms (45.39%), Memory - 48.7 MB (85.80%)

--- 0004-median-of-two-sorted-arrays/solution.java
+++ 0004-median-of-two-sorted-arrays/solution.java
@@ -1,33 +1,29 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
+        int[] new_arr = new int[n];
 
-        int len1 = nums1.length, len2 = nums2.length;
-        int left = 0, right = len1;
+        int i=0, j=0, k=0;
 
-        while (left <= right) {
-            int part1 = (left + right) / 2;
-            int part2 = (len1 + len2 + 1) / 2 - part1;
+        while (i<=n1 && j<=n2) {
+            if (i == n1) {
+                while(j<n2) new_arr[k++] = nums2[j++];
+                break;
+            } else if (j == n2) {
+                while (i<n1) new_arr[k++] = nums1[i++];
+                break;
+            }
 
-            int maxLeft1 = (part1 == 0) ? Integer.MIN_VALUE : nums1[part1 - 1];
-            int minRight1 = (part1 == len1) ? Integer.MAX_VALUE : nums1[part1];
-            int maxLeft2 = (part2 == 0) ? Integer.MIN_VALUE : nums2[part2 - 1];
-            int minRight2 = (part2 == len2) ? Integer.MAX_VALUE : nums2[part2];
-
-            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
-                if ((len1 + len2) % 2 == 0) {
-                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
-                } else {
-                    return (double) Math.max(maxLeft1, maxLeft2);
-                }
-            } else if (maxLeft1 > minRight2) {
-                right = part1 - 1;
+            if (nums1[i] < nums2[j]) {
+                new_arr[k++] = nums1[i++];
             } else {
-                left = part1 + 1;
+                new_arr[k++] = nums2[j++];
             }
         }
-        return 0.0;        
+
+        if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
+        else return new_arr[n/2];
     }
 }
